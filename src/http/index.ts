0908a3077e95ccaefa8f9de9f6172b2949d6a312
arthurgranito <arthurcursotecnico@gmail.com>
2025-01-26 20:16:47 -0300

    commit

--- src/http/index.ts
+++ src/http/index.ts
@@ -13,5 +13,7 @@ export async function obterCategorias(): Promise<ICategoria[]> {
 }
 
 export async function obterReceitas(): Promise<IReceita[]> {
-  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json')
+  return obterDadosURL<IReceita[]>(
+    "https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json"
+  );
 }
